ab0b29c5498f21f7ccbf9dbbee9944952ee150e4
Jennifer Jiyue Wang <jiyue@stanford.edu>
2015-12-29 20:29:09 -0800

    finished all the video

--- project1/trigger_add.sql
+++ project1/trigger_add.sql
@@ -2,7 +2,7 @@ PRAGMA foreign_keys = ON;
 
 -- 8. The Current Price of an item must always match the Amount of the most 
 -- recent bid for that item.
-DROP TRIGGER IF EXISTS update_current_price
+DROP TRIGGER IF EXISTS update_current_price;
 CREATE TRIGGER update_current_price
 AFTER INSERT ON Bids
 FOR EACH ROW
@@ -13,7 +13,7 @@ BEGIN
 END;
 
 -- 9. A user may not bid on an item he or she is also selling.
-DROP TRIGGER IF EXISTS no_sell_and_bid_on_same_item
+DROP TRIGGER IF EXISTS no_sell_and_bid_on_same_item;
 CREATE TRIGGER no_sell_and_bid_on_same_item
 BEFORE INSERT ON Bids
 FOR EACH ROW
@@ -27,7 +27,7 @@ SELECT RAISE(ROLLBACK, 'A user may not bid on an item he or she is also selling.
 END;
 
 -- 10. No auction may have two bids at the exact same time.
-DROP TRIGGER IF EXISTS no_two_bids_at_the_same_time
+DROP TRIGGER IF EXISTS no_two_bids_at_the_same_time;
 CREATE TRIGGER no_two_bids_at_the_same_time
 BEFORE INSERT ON Bids
 FOR EACH ROW
@@ -42,7 +42,7 @@ SELECT RAISE(ROLLBACK, 'No auction may have two bids at the exact same time.');
 END;
 
 -- 11. No auction may have a bid before its start time or after its end time.
-DROP TRIGGER IF EXISTS no_bids_outside_time_interval
+DROP TRIGGER IF EXISTS no_bids_outside_time_interval;
 CREATE TRIGGER no_bids_outside_time_interval
 BEFORE INSERT ON Bids
 FOR EACH ROW
@@ -57,7 +57,7 @@ SELECT RAISE(ROLLBACK, 'No auction may have a bid before its start time or after
 END;
 
 -- 12. No user can make a bid of the same amount to the same item more than once.
-DROP TRIGGER IF EXISTS no_same_bids_for_same_user
+DROP TRIGGER IF EXISTS no_same_bids_for_same_user;
 CREATE TRIGGER no_same_bids_for_same_user
 BEFORE INSERT ON Bids
 FOR EACH ROW
@@ -74,7 +74,7 @@ END;
 
 -- 13. In every auction, the Number of Bids attribute corresponds to the actual number
 -- of bids for that particular item.
-DROP TRIGGER IF EXISTS update_number_of_bids
+DROP TRIGGER IF EXISTS update_number_of_bids;
 CREATE TRIGGER update_number_of_bids
 AFTER INSERT ON Bids
 FOR EACH ROW
@@ -85,7 +85,7 @@ BEGIN
 END;
 
 -- 14.
-DROP TRIGGER IF EXISTS no_less_amount_for_same_item
+DROP TRIGGER IF EXISTS no_less_amount_for_same_item;
 CREATE TRIGGER no_less_amount_for_same_item
 BEFORE INSERT ON Bids
 FOR EACH ROW
@@ -100,7 +100,7 @@ SELECT RAISE(ROLLBACK, 'You should bids for a higher price for this item');
 END;
 
 -- 15.
-DROP TRIGGER IF EXISTS no_mismatch_time
+DROP TRIGGER IF EXISTS no_mismatch_time;
 CREATE TRIGGER no_mismatch_time
 BEFORE INSERT ON Bids
 FOR EACH ROW
@@ -114,7 +114,7 @@ SELECT RAISE(ROLLBACK, 'All new bids must be placed at the time which matches th
 END;
 
 -- 16.
-DROP TRIGGER IF EXISTS no_backward_time
+DROP TRIGGER IF EXISTS no_backward_time;
 CREATE TRIGGER no_backward_time
 BEFORE UPDATE ON CurrentTime
 FOR EACH ROW
